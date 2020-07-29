4b882b594b81933b469243b60b8e910f6c43b430
Mehmet Efe Karadeniz <mehmetefekaradeniz.mek@gmail.com>
2020-07-29 21:35:47 +0300

    new features

--- js/main.js
+++ js/main.js
@@ -3,33 +3,7 @@
  	easing: 'slide',
  	once: false
  });
- (function() {
- 	scrollTo();
- })();
 
- function scrollTo() {
- 	const links = document.querySelectorAll('.smooth');
- 	links.forEach(each => (each.onclick = scrollAnchors));
- }
-
- function scrollAnchors(e, respond = null) {
- 	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
- 	e.preventDefault();
- 	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
- 	const targetAnchor = document.querySelector(targetID);
- 	if (!targetAnchor) return;
- 	const originalTop = distanceToTop(targetAnchor);
- 	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
- 	const checkIfDone = setInterval(function() {
- 		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
- 		if (distanceToTop(targetAnchor) === 0 || atBottom) {
- 			targetAnchor.tabIndex = '-1';
- 			targetAnchor.focus();
- 			window.history.pushState('', '', targetID);
- 			clearInterval(checkIfDone);
- 		}
- 	}, 100);
- }
 jQuery(document).ready(function($) {
 
 	"use strict";
